6f2cce38585cf17638ee4860128007c326eb4e7d
elliottdu17 <77096763+elliottdu17@users.noreply.github.com>
2021-01-07 11:53:54 +0100

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+clear
+apt update
+apt upgrade
+apt install figlet -y
+figlet Basic Installation
+apt install toilet -y
+apt install cowsay -y
+apt install nano -y
+apt install ruby -y
+gem install lolcat
+apt install git curl openssh openssl openssl-tool -y
+figlet -f big FINISHED !!! | lolcat
+echo ""
+echo ""
+echo ""
+sleep 10 
+echo ""
+sleep 0
+bash instacracker.sh
